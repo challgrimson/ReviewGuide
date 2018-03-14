bd696283c582341309109cb7de06c4bb82a9a576
neetshah <shahn17@mcmaster.ca>
2018-03-14 18:24:55 -0400

    Added insert to create Product objects

---
+++ src/xb3_project/Insert.java
@@ -0,0 +1,15 @@
+package xb3_project;
+
+import java.util.Hashtable;
+
+public class Insert {
+	private static Hashtable ht = new Hashtable();
+	
+	public static void create(Product[] x) {
+		for(int i = 0; i<x.length; i++)
+			ht.put(x[i].getProductID(), x[i]);
+	}
+	public static Product getRatings(Product x) {
+		return (Product) ht.get(x.getProductID());
+	}
+}
