42e07f3656aacf83a0616b1e14b16f8b5406eed9
christian <hallgric@mcmaster.ca>
2018-03-27 16:54:18 -0400

    fixed crap

--- src/xb3_project/linkedBag.java
+++ src/xb3_project/linkedBag.java
@@ -1,14 +1,21 @@
-public class Bag<Product> {
+package xb3_project;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+//christian
+//accessing the items in the array is done through the iterator class at the bottom, easiest way, let me know if needs more functionality
+public class linkedBag {
    
-    private Node<Product> first;    // beginning of bag
+    private Node first;    // beginning of bag
     private int n;               // number of elements in bag
 
-    private static class Node<Product> {
-        private Product product;
-        private Node<Product> next;
+    private static class Node {
+        private String product;
+        private Node next;
     }
 
-    public Bag() {
+    public linkedBag() {
         first = null;
         n = 0;
     }
@@ -21,11 +28,40 @@ public class Bag<Product> {
         return n;
     }
 
-    public void add(Product Product) {
-        Node<Product> oldfirst = first;
-        first = new Node<Product>();
-        first.item = item;
+    public void add(String product) {
+        Node oldfirst = first;
+        first = new Node();
+        first.product = product;
         first.next = oldfirst;
         n++;
+    }
+    
+    
+    //sedgewick
+    public Iterator<String> iterator()  {
+        return new ListIterator();  
+    }
+
+    // an iterator over a linked list
+    private class ListIterator implements Iterator<String>{
+        private Node current;
+
+        public ListIterator() {
+            current = first;
+        }
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        // returns the next item in the iterator (and advances the iterator)
+        public String next() {
+            if (!hasNext()) 
+            	throw new NoSuchElementException();
+            
+            String product = current.product;
+            current = current.next; 
+            return product;
+        }
     }
 }
